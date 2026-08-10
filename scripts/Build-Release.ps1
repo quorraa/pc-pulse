6a9edefea6acf2f28f742d53a6b7b8abdca065f4
quorraa <x-avier@live.com>
2026-08-10 01:30:57 -0400

    Release PC Pulse v1.10.0

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -2,7 +2,7 @@
 param(
     [ValidateSet('x64')]
     [string]$Architecture = 'x64',
-    [string]$Version = '1.9.1',
+    [string]$Version = '1.10.0',
     [string]$CertificateThumbprint,
     [string]$TimestampUrl = 'http://timestamp.digicert.com',
     [switch]$SkipTests
